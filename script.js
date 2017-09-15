f7b56301ef80429ad1917f197d150b1044da56f5
neilburd <neilburd@gmail.com>
2017-09-15 15:13:45 -0400

    added the create array function to the script file and dropped trying to import for now

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-//import "shuffled.js"
-
 $(document).ready(function() {
+
+  let pairs = [];
+
+  //function to test if number is Even
+  function isEven(n) {
+    return n == parseFloat(n)? !(n%2) : void 0;
+  }
+  /// CREATE PAIRED ARRAY AT CERTAIN length
+  function createBoard(x){
+    let total = x;
+    let halfT;
+
+    if (isEven(x))
+      halfT = total/2;
+      let x=1;
+      for (i=0; i < halfT; i++){
+        pairs.push(x,x);
+        x++;
+      }
+    }
+
+
+    shuffle(pairs);
+
+
+  function shuffle(a) {
+      var j, x, i;
+      for (i = a.length; i; i--) {
+          j = Math.floor(Math.random() * i);
+          x = a[i - 1];
+          a[i - 1] = a[j];
+          a[j] = x;
+      }
+  }
+
 
   for (i = 1; i < 17; i++){
     console.log(i);
