a688aff0eaf274c72b1c7e57d58de46a4431ab4e
neilburd <neilburd@gmail.com>
2017-09-18 20:52:59 -0400

    I got the match working

--- script.js
+++ script.js
@@ -1,76 +1,121 @@
 $(document).ready(function() {
 
-  let pairs = [];
+  let pairsArray = [];
+  let tilesId = [];
+  let matches = [];
+  let tilesFlipped = 0;
+
 
   $( "form" ).submit(function( event ) {
   event.preventDefault();
   });
 
   $('#gridSubmit').click(function() {
-
-    pairs = [];
+    //RESET EVERYTHING
+    pairsArray = [];
+    tilesId = [];
+    matches = [];
+    tilesFlipped = 0;
     $('.game').empty();
 
-
+    // get the size of the board
     let boardSize = $("input[name='gridSize']:checked").val();
-
-    // console.log(boardSize);
-    createBoard(boardSize);
-    // console.log(pairs);
-
-    for (i = 0; i < pairs.length; i++){
+    // shuffle the array and fill the game div with the items
+    shuffleArray(boardSize);
+    for (i = 0; i < pairsArray.length; i++){
       // console.log(i);
-      $(".game").append(`<div class="item" data-gridSize="${pairs[i]}">${pairs[i]}</div>`)
+      $(".game").append(`<div class="item" data-gridSize="${pairsArray[i]}">${pairsArray[i]}</div>`)
     }
   });
-
-  let match = null;
 
 ////ALL THE CLICK AND GAME PLAY
 
   $(".game").on("click",".item",function(){
+
     let $this = $(this);
     let number = $this.attr('data-gridSize');
 
     console.log($this);
-    $this.addClass('.reveal');
+    $this.addClass('reveal');
 
-    if (match === null){
-      match = number;
-      //make this square unclickable after clicked once
-    } else if (match === number) {
-      console.log("match");
-      //this is where i put the actions for a match
+    // tilesId = [];
+    // matches = [];
+    // tilesFlipped = 0;
 
-      match = null;
-    } else {
-      console.log('NO match');
-      /// this is where I put the actions for no match
+    // //make this square unclickable after clicked once
+    // $this.click(false);
 
-      match = null;
+
+    ////CHECK FOR MATCH
+    /// if htere are less than 2 in the match arrray
+    if (matches.length < 2){
+
+      if (matches.length === 0){
+        matches.push(number);
+        /// do I need the ID
+        console.log("first item")
+      } else if (matches.length === 1){
+        matches.push(number);
+        /// do I need the ID
+        console.log("second item")
+        if (matches[0] === matches[1]){
+          tilesFlipped +=2;
+          matches = [];
+          console.log("MATCH")
+          if (tilesFlipped === pairsArray.length){
+            alert("YOU WON");
+          }
+        } else {
+          matches = [];
+          console.log("NO MATCH")
+        }
+      }
+
+
     }
+  });
+  //   ///// MATCH /////
+  //   else if (match === number) {
+  //     //this is where i put the actions for a match
+  //
+  //     console.log("second click ---- match");
+  //     ///make this one unclickable
+  //     $this.click(false);
+  //
+  //     $('.item').each(function(index, val) {
+  //       ///val is the entire html line
+  //
+  //       console.log($(this).attr('data-gridSize'));
+  //       if ($(this).attr('data-gridSize') === number){
+  //         //add class to all the items that match the number??
+  //         $(this).addClass('match');
+  //       }
+  //
+  //     });
+  //
+  //     match = null;
+  //   } else {
+  //     console.log('second click ----- NO match');
+  //     /// this is where I put the actions for no match
+  //     // will have to rebind the click to each item
+  //
+  //     match = null;
+  //   }
+  //
+  // })
 
-  })
 
-  function isMatch(a,b){
-    if (a === b){
-      return true
-    }
-    else return false
-  }
 
 
   function clearAll() {
-
-    // console.log("remove" + i);
-    pairs = [];
+    pairsArray = [];
   }
   //function to test if number is Even
   function isEven(n) {
     return n == parseFloat(n)? !(n%2) : void 0;
   }
   /// CREATE PAIRED ARRAY AT CERTAIN length
-  function createBoard(total){
+  function shuffleArray(total){
 
     let halfT;
 
@@ -78,11 +123,11 @@ $(document).ready(function() {
       halfT = total/2;
       let x=1;
       for (i=0; i < halfT; i++){
-        pairs.push(x,x);
+        pairsArray.push(x,x);
         x++;
       }
 
-      shuffle(pairs);
+      shuffle(pairsArray);
 
     }
   function shuffle(a) {
