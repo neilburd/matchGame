764f522b32c5a3f3f124cb2ed532cd41b989def3
neilburd <neilburd@gmail.com>
2017-09-25 20:58:18 -0400

    trying to work on making this all vanilla Js

--- script-vanilla.js
+++ script-vanilla.js
@@ -1,28 +1,20 @@
-$(document).ready(function() {
-
+// $(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
+  // code...
   let pairsArray = [];
   let tilesId = [];
   let matches = [];
   let tilesFlipped = 0;
 
+  document.getElementById('gridSubmit').addEventListener('click', function(){
+    console.log("CLICK");
+    event.preventDefault();
 
-  $( "form" ).submit(function( event ) {
-  event.preventDefault();
-  });
-
-
-  //$('#gridSubmit').click(function() {
-    //RESET EVERYTHING
-document.getElementById('gridSubmit').addEventListener('click', gridSubmit);
-
-function gridSubmit(){
-  console.log("clicked");
-}
     pairsArray = [];
     tilesId = [];
     matches = [];
     tilesFlipped = 0;
-    $('.game').empty();
+    // $('.game').empty();
 
     // get the size of the board
     let boardSize = $("input[name='gridSize']:checked").val();
@@ -32,106 +24,127 @@ function gridSubmit(){
       // console.log(i);
       $(".game").append(`<div class="item" id="tileId_${i}" data-gridSize="${pairsArray[i]}">${pairsArray[i]}</div>`)
     }
+
+
   });
 
-////ALL THE CLICK AND GAME PLAY
 
-  $(".game").on("click",".item",function(){
-
-    let $this = $(this);
-    let number = $this.attr('data-gridSize');
-    let tile_Id = event.target.id;
-
-    //console.log(event.target.id);
-
-    $this.addClass('reveal');
-
-    ////CHECK FOR MATCH
-    /// if htere are less than 2 in the match arrray
-    if (matches.length < 2){
-
-      if (matches.length === 0){
-        matches.push(number);
-        tilesId.push('#' + tile_Id);
-
-        console.log("first item")
-
-      } else if (matches.length === 1){
-        matches.push(number);
-        tilesId.push('#' + tile_Id);
-
-        console.log("second item");
-        ///MATCH
-        if (matches[0] === matches[1]){
-          tilesFlipped +=2;
-
-          matches = [];
-          tilesId = [];
-
-          console.log("MATCH")
-          /// CHECK FOR END OF GAME
-          if (tilesFlipped === pairsArray.length){
-            alert("YOU WON");
-          }
-        }
-        //// NO MATCH
-        else {
-
-          setTimeout(flipBack,500);
-
-          console.log(tilesId);
-        }
-      }
+  // $( "form" ).submit(function( event ) {
+  // event.preventDefault();
+  // });
 
 
-    }
-  });
-
-  function flipBack(){
-    console.log("flip Back");
-    //console.log(tilesId[0]);
-    $(tilesId[0]).removeClass('reveal');
-
-    //console.log(tilesId[1]);
-    $(tilesId[1]).removeClass('reveal');
-
-    matches = [];
-    tilesId = [];
-
-  }
-
-  function clearAll() {
-    pairsArray = [];
-  }
-  //function to test if number is Even
-  function isEven(n) {
-    return n == parseFloat(n)? !(n%2) : void 0;
-  }
-  /// CREATE PAIRED ARRAY AT CERTAIN length
-  function shuffleArray(total){
-
-    let halfT;
-
-    if (isEven(total))
-      halfT = total/2;
-      let x=1;
-      for (i=0; i < halfT; i++){
-        pairsArray.push(x,x);
-        x++;
-      }
-
-      shuffle(pairsArray);
-
-    }
-  function shuffle(a) {
-      var j, x, i;
-      for (i = a.length; i; i--) {
-          j = Math.floor(Math.random() * i);
-          x = a[i - 1];
-          a[i - 1] = a[j];
-          a[j] = x;
-      }
-  }
-
+  //$('#gridSubmit').click(function() {
+    //RESET EVERYTHING
+//
+//
+//     // get the size of the board
+//     let boardSize = $("input[name='gridSize']:checked").val();
+//     // shuffle the array and fill the game div with the items
+//     shuffleArray(boardSize);
+//     for (i = 0; i < pairsArray.length; i++){
+//       // console.log(i);
+//       $(".game").append(`<div class="item" id="tileId_${i}" data-gridSize="${pairsArray[i]}">${pairsArray[i]}</div>`)
+//     }
+//   // });
+//
+// ////ALL THE CLICK AND GAME PLAY
+//
+//   $(".game").on("click",".item",function(){
+//
+//     let $this = $(this);
+//     let number = $this.attr('data-gridSize');
+//     let tile_Id = event.target.id;
+//
+//     //console.log(event.target.id);
+//
+//     $this.addClass('reveal');
+//
+//     ////CHECK FOR MATCH
+//     /// if htere are less than 2 in the match arrray
+//     if (matches.length < 2){
+//
+//       if (matches.length === 0){
+//         matches.push(number);
+//         tilesId.push('#' + tile_Id);
+//
+//         console.log("first item")
+//
+//       } else if (matches.length === 1){
+//         matches.push(number);
+//         tilesId.push('#' + tile_Id);
+//
+//         console.log("second item");
+//         ///MATCH
+//         if (matches[0] === matches[1]){
+//           tilesFlipped +=2;
+//
+//           matches = [];
+//           tilesId = [];
+//
+//           console.log("MATCH")
+//           /// CHECK FOR END OF GAME
+//           if (tilesFlipped === pairsArray.length){
+//             alert("YOU WON");
+//           }
+//         }
+//         //// NO MATCH
+//         else {
+//
+//           setTimeout(flipBack,500);
+//
+//           console.log(tilesId);
+//         }
+//       }
+//
+//
+//     }
+//   });
+//
+//   function flipBack(){
+//     console.log("flip Back");
+//     //console.log(tilesId[0]);
+//     $(tilesId[0]).removeClass('reveal');
+//
+//     //console.log(tilesId[1]);
+//     $(tilesId[1]).removeClass('reveal');
+//
+//     matches = [];
+//     tilesId = [];
+//
+//   }
+//
+//   function clearAll() {
+//     pairsArray = [];
+//   }
+//   //function to test if number is Even
+//   function isEven(n) {
+//     return n == parseFloat(n)? !(n%2) : void 0;
+//   }
+//   /// CREATE PAIRED ARRAY AT CERTAIN length
+//   function shuffleArray(total){
+//
+//     let halfT;
+//
+//     if (isEven(total))
+//       halfT = total/2;
+//       let x=1;
+//       for (i=0; i < halfT; i++){
+//         pairsArray.push(x,x);
+//         x++;
+//       }
+//
+//       shuffle(pairsArray);
+//
+//     }
+//   function shuffle(a) {
+//       var j, x, i;
+//       for (i = a.length; i; i--) {
+//           j = Math.floor(Math.random() * i);
+//           x = a[i - 1];
+//           a[i - 1] = a[j];
+//           a[j] = x;
+//       }
+//   }
 
 });
